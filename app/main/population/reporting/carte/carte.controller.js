39f628fa4359e7b0f3e9507923706bb82175ac29
Harizo <rajaonarisoazo@gmail.com>
2020-04-30 11:01:17 +0300

    carte

--- app/main/population/reporting/carte/carte.controller.js
+++ app/main/population/reporting/carte/carte.controller.js
@@ -9,100 +9,327 @@
     /** @ngInject */
     function carteController($scope, $mdDialog, apiFactory,$state, apiUrlExcel, uiGmapGoogleMapApi,$cookieStore)
     {
+      var vm = this ;
+
+      //recuperation region
+      apiFactory.getAll("region/index").then(function(result)
+      {
+          vm.allregion = result.data.response;    
+      });
+
+      //recuperation district par region
+        vm.modifierregion = function(filtre)
+        {   
+            vm.allcommune = [] ;
+            apiFactory.getAPIgeneraliserREST("district/index","cle_etrangere",filtre.region_id).then(function(result)
+            {
+              vm.filtre_carte.district_id = '*' ;
+              vm.filtre_carte.commune_id = '*' ;
+              vm.alldistrict = result.data.response;
+              vm.isDistrict = true;
+            });
+        }
+
+        
+
+        vm.pivots = [
+          
+         /* {
+            titre:"* Nombre cumulé bénéficiaire_33",
+            id:"req33theme2_interven_nbrbenef_region_dist_comm",
+            category:"theme2"
+
+          }
+          //DEBUT CODE HARIZO
+          ,*/
+          {
+            titre:"Effectif des bénéficiaires handicapés",
+            id:"req40_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Effectif des bénéficiaires sortis du programme",
+            id:"req41_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Moyenne des transferts",
+            id:"req42_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Total des transferts",
+            id:"req43_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Répartition décaissement par programme",
+            id:"req10_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Répartition décaissement par tutelle",
+            id:"req11_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Répartition des agences d'exécution par programme",
+            id:"req12_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Montant du budget non consommé",
+            id:"req37_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Taux de décaissement par intervention",
+            id:"req36_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Proportion des interventions par type de cible",
+            id:"req18_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Répartition géographique des interventions",
+            id:"req14_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Proportion des interventions avec critères de sexe",
+            id:"req20_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Proportion des interventions avec critères d'âge",
+            id:"req19_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Nombre de nouveaux bénéficiaires",
+            id:"req32_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Taux d’atteinte des résultats",
+            id:"req34_theme2",
+            category:"theme2"
+          },
+          ,
+          {
+            titre:"Cible de l'intervention",
+            id:"req_multiple_21_to_30_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Indice de niveau de vulnérabilité",
+            id:"req6_theme2",
+            category:"theme2"
+          }/*,
+
+          {
+            titre:"Listes bénéficiaires",
+            id:"liste_beneficiaire_intevention",
+            category:"theme1"
+          }*/, 
+
+          //FIN CODE HARIZO    
+
+          //code modifier par harizo
+          { 
+            titre:"Effectif par age sexe de la population",
+            id:"req1_theme1",
+            category:"theme1"
+          },
+          { 
+            titre:"Effectif menage ayant enfant",
+            id:"req3_theme1",
+            category:"theme1"
+          },
+          { 
+            titre:"Répartition par âge et par sexe des bénéficiaires",
+            id:"req38_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Répartition financement par programme",
+            id:"req7_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Répartition financement par source",
+            id:"req8_theme2",
+            category:"theme2"
+          },
+          {
+            titre:"Répartition financement par tutele",
+            id:"req9_theme2",
+            category:"theme2"
+          }    
+          //code modifier par harizo   
+          
+          //Debut Bruce
+          ,
+          /*{
+            titre:"* Nombre des bénéficiaires prévus_31",
+            id:"req31theme2_interven_nbrinter_program_beneparan_beneprevu_region",
+            category:"theme2"
+          }*//*,
+          {
+            titre:"Proportion des interventions avec critères d'âge",
+            id:"req19theme2_interven_pourcenenfan_pourcensco_pourcentra_pourcenage_pcout",
+            category:"theme2"
+          }*/    
+          //Fin Bruce
+        
+        ];
+
 
       var vm = this ;
         uiGmapGoogleMapApi.then(function (maps)
         {
-
+          vm.map = maps ;
+          //console.log("version carte == " +maps.version);
           vm.map = 
           {
               center: {
                   latitude : -18.881728,
                   longitude: 47.510447
               },
-              zoom  : 6/*,
-              events: { 
-                  click: function(latLng)
-                  { 
-                  console.log(latLng); 
+              zoom  : 6,
+              events: {
+                    tilesloaded: function (map) {
+                        $scope.$apply(function () {
+                              // console.info('this is the map instance', map);
 
-                  },
-                  mouseover: function(gPoly, eventName, polyModel, latLngArgs) {
-                                       console.log("mouseover ok");
-                  },
-                  mouseout: function(gPoly, eventName, polyModel, latLngArgs) {
+                        });
+                    },
+                    click: function(map, eventName,latLngArgs)
+                    { 
 
-                    console.log("mouseout ok");
-                    console.log(latLngArgs);
-                  }
-                }*/
-              //marker: vm.liste
+
+                      var pts = 
+                      {
+                        lat: latLngArgs[0].latLng.lat(), 
+                        lng: latLngArgs[0].latLng.lng()
+                      };
+
+                      map.setCenter(pts);
+                      //map.setZoom(10); 
+
+                    }
+                }
           };
 
-          vm.sss = function(){console.log('okokok');}
+
+          vm.windowOptions = {
+              visible: true 
+          };
 
           var id_user = $cookieStore.get('id');
+          vm.polylines = [];   
 
-          apiFactory.getAll("commune/index").then(function(result) 
+          
+
+          vm.affichage_commune_in_carte = function()
           {
-            console.log(result.data.response);  
-            vm.polylines = [];    
 
-
-
-            vm.all_commune = result.data.response ;
-
-            angular.forEach(vm.all_commune, function(value, key)
+            //apiFactory.getAll("commune/index").then(function(result) 
+            apiFactory.getAPIgeneraliserREST("commune/index","cle_etrangere",vm.filtre_carte.district_id).then(function(result)
             {
-              
+              console.log(result.data.response);  
+               
 
-              var item = 
+
+
+              vm.all_commune = result.data.response ;
+
+              angular.forEach(vm.all_commune, function(value, key)
               {
-                id: value.id ,
-                path:value.coordonnees,
-                stroke: {
-                    color: '#7f421e',
-                    weight: 2
-                },                  
-                fill: {
-                    
-                    opacity: 0,
-                    color:'#f2650e'
-                     
-                },
-                events: { 
-                  click: function(event)
-                  { 
-                  console.log(value.id); 
 
+                
+                
+
+                var item = 
+                {
+                  id: value.id ,
+                  id_mark: "mark_"+value.id ,
+                  id_win: "win"+value.id ,
+                  nom: value.nom ,
+                  path:value.coordonnees,
+                  marker_cord:vm.get_center_poly(value.coordonnees),
+                  marker_option:{
+                    icon:'assets/icons/mark.png'
                   },
-                  mouseover: function(gPoly, eventName, polyModel, latLngArgs) {
-                    polyModel.fill.opacity = '0.5';
-                   console.log("mouseover ok");
+                  stroke: {
+                      color: '#7f421e',
+                      weight: 2
+                  },                  
+                  fill: {
+                      
+                      opacity: 0,
+                      color:'#f2650e'
+                       
                   },
-                  mouseout: function(gPoly, eventName, polyModel, latLngArgs) {
-                    polyModel.fill.opacity = '0';
-                    console.log("mouseout ok");
-                    console.log(latLngArgs);
+                  events: { 
+                    click: function(event)
+                    { 
+                    console.log(value.id); 
+
+                    },
+                    mouseover: function(gPoly, eventName, polyModel, latLngArgs) {
+                      polyModel.fill.opacity = '0.5';
+                    // console.log(JSON.stringify(polyModel.path));
+                    },
+                    mouseout: function(gPoly, eventName, polyModel, latLngArgs) {
+                      polyModel.fill.opacity = '0';
+                      console.log("mouseout ok");
+                      console.log(latLngArgs);
+                    }
                   }
-                }
-              };
+                };
 
-            
-                vm.polylines.push(item);
               
-              console.log(vm.polylines);
-              
+                  vm.polylines.push(item);
+                
+                console.log(vm.polylines);
+                
+              });
+
+
             });
 
-
-          });
+          }
 
       });
 
-        
 
-    
+      vm.get_center_poly = function(path)
+      {
+        
+        var centroid = [];
+         centroid[0] = 0.0 ;
+         centroid[1] = 0.0 ;
+        path.forEach( function(element, index) {
+          centroid[0] += Number(element.latitude);
+            centroid[1] += Number(element.longitude);
+        });
+
+        var totalPoints = path.length;
+        centroid[0] = centroid[0] / totalPoints;
+        centroid[1] = centroid[1] / totalPoints;
+
+        var pts = {
+          latitude : centroid[0],
+          longitude : centroid[1]
+        }
+
+        return pts;
+
+      }
+
 
     }
 })();
--- app/main/population/reporting/carte/carte.module.js
+++ app/main/population/reporting/carte/carte.module.js
@@ -7,7 +7,7 @@
         .config(config);
 
     /** @ngInject */
-    function config($stateProvider, $translatePartialLoaderProvider, msNavigationServiceProvider)
+    function config($stateProvider, $translatePartialLoaderProvider, msNavigationServiceProvider,uiGmapGoogleMapApiProvider)
     {
         // State
         $stateProvider.state('app.population_reporting_carte', {
@@ -33,6 +33,13 @@
             state: 'app.population_reporting_carte',
             weight: 2
         });
+
+        uiGmapGoogleMapApiProvider.configure({
+            key: 'AIzaSyCSbNSIOle6qaecQtJ68K-gdyiiLHnQdb4&callback',
+        v: '3.20', //defaults to latest 3.X anyhow
+        //libraries: 'weather,geometry,visualization'
+        libraries: 'geometry,visualization'
+    });
     }
 
 })();
